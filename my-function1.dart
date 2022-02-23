3a15fbfaeee4622e128a3ac95699e35f9fa8ac16
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-02-23 16:22:21 +0530

    Create my-function1.dart

---
+++ my-function1.dart
@@ -0,0 +1,12 @@
+void main() {
+  double val = checkFun();
+  if (val>10) {
+    print('Value is $val');
+  } else {
+    print('Value is less than 10!');
+  }
+}
+double checkFun() {
+  double chkVal = 25.98;
+  return chkVal;
+}
