915d47eb1774d7321b1e64da94a8b0a1dedec181
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-02-24 22:08:21 +0530

    Create my-list.dart

---
+++ my-list.dart
@@ -0,0 +1,6 @@
+void main(){
+  List<String> colours = ['red', 'blue', 'yellow', 'green', 'orange'];
+  print ('Colours: $colours');
+  print ('0: $colours[0]');
+  print ('No of colours: ${colours.length}');
+}
